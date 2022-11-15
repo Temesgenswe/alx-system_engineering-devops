184ccebc6deeb2dea798f87968147994f6dbdc95
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-11-15 05:35:41 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""Return information about his/her TODO list progress"""
+"""
+    using a REST API, for a given employee ID
+    returns information about his/her TODO list progress
+"""
+
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    user_id = argv[1]
-
-    url = "https://jsonplaceholder.typicode.com/"
-    uri_user_id = "users/{}".format(user_id)
-    uri_todos = "todos"
-
-    user = requests.get(url + uri_user_id).json()
-    tasks = requests.get(url + uri_todos, params={"userId": user_id}).json()
-
-    tasks_completed = [task for task in tasks if task.get("completed") is True]
-
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                                                          len(tasks_completed),
-                                                          len(tasks)))
-
-    [print("\t {}".format(task.get("title"))) for task in tasks_completed]
+    if len(argv) > 1:
+        userId = int(argv[1])
+        url = "https://jsonplaceholder.typicode.com/users/"
+        r = requests.get("{}/{}".format(url, userId))
+        data = r.json()
+        name = r.json().get("name")
+        if name is not None:
+            userTodos = requests.get("{}{}/todos".format(url, userId))
+            userTodos = userTodos.json()
+            taskNumber = len(userTodos)
+            completedTasks = []
+            for task in userTodos:
+                if task.get("completed") is True:
+                    completedTasks.append(task)
+            taskCompletedNumber = len(completedTasks)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, taskCompletedNumber, taskNumber))
+            for task in completedTasks:
+                title = task.get("title")
+                print("\t {}".format(title))
