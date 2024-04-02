5a12bd9a33b198dc012e64f146e3879127008cd3
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2024-04-02 09:16:49 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,13 @@
-#Puppet file that fixes multiple server errors in Nginx server
+# Increases the amount of traffic an Nginx server can handle.
 
-exec { 'fix_limit':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  =>  "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 3072\"/g' /etc/default/nginx; sudo service nginx restart",
-  provider =>  'shell'}
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
