d2b770dff5cf60f174da3df4baebea68219d188a
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2024-04-13 13:24:55 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""
-queries the Reddit API and prints the titles of
-the first 10 hot posts listed for a given subreddit.
-"""
 import requests
+"""
+functions that queries the reddit api
+"""
 
 
 def top_ten(subreddit):
     """
-    prints the titles of the first 10 hot posts listed for
-    a given subreddit
+    function definition that prints the titles of first 10 hot posts listed
+    for a given subreddit
     """
-    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
+    url = "https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit)
+    header = {'User-Agent': 'CustomClient/1.0'}
+    req = requests.get(url, headers=header, allow_redirects=False)
+    if req.status_code != 200:
         print(None)
         return
-    response = response.json()
-    if 'data' in response:
-        for posts in response.get('data').get('children'):
-            print(posts.get('data').get('title'))
-
+    req = req.json()
+    if "data" in req:
+        for post in req.get("data").get("children"):
+            print(post.get("data").get("title"))
     else:
         print(None)
