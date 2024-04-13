150a14f724cd8184523c52ed0c1b69f36747c8be
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2024-04-13 13:23:24 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
+import requests
 """
-number of subscribers for a given subreddit
+function that queries reddit api and returns number
+of subs
 """
-
-from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    function definition of getting number of subs
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    header = {'User-Agent': 'CustomClient/1.0'}
+    req = requests.get(url, headers=header, allow_redirects=False)
+    if req.status_code != 200:
+        return (0)
+    req = req.json()
+    if "data" in req:
+        return (req.get("data").get("subscribers"))
+    else:
+        return (0)
